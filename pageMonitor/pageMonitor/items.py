f61e7c051d1d52f95c15ece15118857c8d8e6496
sddtc <changhbaga@gmail.com>
2016-07-25 14:22:30 +0800

    add something about price but it can not run

--- pageMonitor/pageMonitor/items.py
+++ pageMonitor/pageMonitor/items.py
@@ -7,11 +7,7 @@
 
 import scrapy
 
-
 class PagemonitorItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     title = scrapy.Field()
     price = scrapy.Field()
     oriPrice = scrapy.Field()
-    url = scrapy.Field()
--- pageMonitor/pageMonitor/spiders/pagemonitor_spider.py
+++ pageMonitor/pageMonitor/spiders/pagemonitor_spider.py
@@ -2,10 +2,14 @@
 import scrapy
 
 class PagemonitorSpider(scrapy.spiders.Spider):
-	name = "pagemonitor"
-	allowed_domains = ["zara.cn"]
-	start_urls = ['http://www2.hm.com/zh_cn/productpage.0373764011.html','https://detail.tmall.com/item.htm?spm=a1z10.4-b.w4007-14252748703.1.06pqbA&id=530545346188&rn=a9af89d7bee15111279bf6b04e840a85&abbucket=5','http://www.zara.cn/cn/zh/%E6%89%93%E6%8A%98/trf/%E5%A4%96%E5%A5%97%E5%92%8C%E4%B8%8A%E8%A1%A3/%E9%92%A9%E9%92%88%E5%BC%80%E8%A5%9F%E8%A1%AB-c436441p3351008.html']
+    name = "pagemonitor"
+    allowed_domains = ["zara.cn"]
+    start_urls = ['http://www2.hm.com/zh_cn/productpage.0373764011.html']
+                    #'https://detail.tmall.com/item.htm?spm=a1z10.4-b.w4007-14252748703.1.06pqbA&id=530545346188&rn=a9af89d7bee15111279bf6b04e840a85&abbucket=5',
+                    #'http://www.zara.cn/cn/zh/%E6%89%93%E6%8A%98/trf/%E5%A4%96%E5%A5%97%E5%92%8C%E4%B8%8A%E8%A1%A3/%E9%92%A9%E9%92%88%E5%BC%80%E8%A5%9F%E8%A1%AB-c436441p3351008.html']
 
-	def parse(self, response):
-		filename = response.url.split("/")[-2]
-		print '====>' + filename
+    def parse(self, response):
+        title = selector.xpath('//h1[@class="product-item-headline"]/text()').extract()
+        price = sel.xpath('//section[contains(@class,"product-detail-meta")]//span[@class="price-value"]/text()').extract()
+        oriprice = sel.xpath('//section[contains(@class,"product-detail-meta")]//small[@class="price-value-original"]/text()').extract()
+
